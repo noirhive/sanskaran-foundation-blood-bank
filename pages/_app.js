07a508989429050c238afe4a7110a99da45d385d
NoirHive <saimunsajal12@gmail.com>
2025-10-23 04:05:42 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import "@fontsource/hind-siliguri";
+import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
